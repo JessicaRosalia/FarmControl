8b94da2c11497913c51b96894b867e98393dc4a5
Vinicius <vinicius.lopes@ufersa.edu.br>
2020-12-04 15:10:44 -0300

    remocao de import desnecessário

--- src/br/com/farmcontrol/model/dao/OvoDao.java
+++ src/br/com/farmcontrol/model/dao/OvoDao.java
@@ -13,7 +13,6 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
-import org.postgresql.util.PSQLException;
 
 public class OvoDao {
     
