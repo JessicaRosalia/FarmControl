952b09a3fe48b91502b6e6608a3712fef7b428dc
Vinicius <vinicius.lopes@ufersa.edu.br>
2020-12-04 15:34:31 -0300

    inicio da classe AnimalDAO

--- src/br/com/farmcontrol/model/dao/AnimalDAO.java
+++ src/br/com/farmcontrol/model/dao/AnimalDAO.java
@@ -1,5 +1,38 @@
 package br.com.farmcontrol.model.dao;
 
+import br.com.farmcontrol.connection.ConnectionFactory;
+import br.com.farmcontrol.model.vo.Animal;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 public class AnimalDAO {
 
+        public static void create(Animal a){
+        
+        Connection con = ConnectionFactory.getConnection();
+        String sql = "INSERT INTO animal(raca,data_nasc_aqui,data_venda,valor_venda) "
+                + "VALUES(?,?,?,?)";
+        PreparedStatement stmt = null;
+        
+        try {
+            
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, a.getRaca());
+            stmt.setDate(2, (Date) a.getData_nasc_aquisicao());
+            stmt.setDate(3, (Date) a.getData_venda());
+            stmt.setFloat(4, a.getValor_venda());
+            stmt.executeUpdate();
+            JOptionPane.showConfirmDialog(null, "Salvo com sucesso!");
+            
+        } catch (SQLException ex) {
+            JOptionPane.showConfirmDialog(null, " Erro ao salvar: "+ex);
+        }finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+        
+    }
+    
 }
---
+++ src/br/com/farmcontrol/model/dao/AnimalDAODemo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.farmcontrol.model.dao;
+
+import br.com.farmcontrol.model.vo.Animal;
+import br.com.farmcontrol.model.vo.LoteAves;
+import br.com.farmcontrol.model.vo.Mamifero;
+
+/**
+ *
+ * @author viniciuslopes
+ */
+public class AnimalDAODemo {
+ 
+    Animal ani = new Mamifero();
+    
+    
+}
