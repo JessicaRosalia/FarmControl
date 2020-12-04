49ac6876576094f64c3296d1047c17f404ac103b
Vinicius <vinicius.lopes@ufersa.edu.br>
2020-12-04 16:43:26 -0300

    Classe AnimalDAO quase pronta

--- src/br/com/farmcontrol/model/dao/AnimalDAO.java
+++ src/br/com/farmcontrol/model/dao/AnimalDAO.java
@@ -2,10 +2,17 @@ package br.com.farmcontrol.model.dao;
 
 import br.com.farmcontrol.connection.ConnectionFactory;
 import br.com.farmcontrol.model.vo.Animal;
+import br.com.farmcontrol.model.vo.LoteAves;
+import br.com.farmcontrol.model.vo.Mamifero;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class AnimalDAO {
@@ -13,7 +20,7 @@ public class AnimalDAO {
         public static void create(Animal a){
         
         Connection con = ConnectionFactory.getConnection();
-        String sql = "INSERT INTO animal(raca,data_nasc_aqui,data_venda,valor_venda) "
+        String sql = "INSERT INTO animal(raca,data_nasc_aqui,dat_venda,valor_venda) "
                 + "VALUES(?,?,?,?)";
         PreparedStatement stmt = null;
         
@@ -34,5 +41,79 @@ public class AnimalDAO {
         }
         
     }
-    
+        
+        public static List<Animal> read(){
+            Connection con = ConnectionFactory.getConnection();
+            String sql = "SELECT * FROM animal";
+            PreparedStatement stmt = null;
+            ResultSet rs = null;
+
+            List<Animal> animais = new ArrayList<>();
+
+            try {
+                stmt = con.prepareStatement(sql);
+                rs = stmt.executeQuery();
+
+                while(rs.next()){
+                    Animal a = new Mamifero();
+                    a.setId_animal(rs.getInt("idanimal"));
+                    a.setRaca(rs.getString("raca"));
+                    a.setData_nasc_aquisicao(rs.getDate("data_nasc_aqui"));
+                    a.setData_venda(rs.getDate("dat_venda"));
+                    a.setValor_venda(rs.getFloat("valor_venda"));
+                    animais.add(a);
+                }
+
+            } catch (SQLException ex) {
+                Logger.getLogger(OvoDao.class.getName()).log(Level.SEVERE, null, ex);
+            } finally{
+                ConnectionFactory.closeConnection(con, stmt, rs);
+            }
+
+            return animais;
+        }
+        
+        public static void update(Animal a){
+        Connection con = ConnectionFactory.getConnection();
+        String sql = "UPDATE animal "
+                   + "SET raca=?,data_nasc_aqui=?,dat_venda=?,valor_venda=?"
+                   + "WHERE idanimal=?";
+        PreparedStatement stmt = null;
+        
+        try {
+            
+            stmt = con.prepareStatement(sql);
+            stmt.setString(1, a.getRaca());
+            stmt.setDate(2, (Date) a.getData_nasc_aquisicao());
+            stmt.setDate(3, (Date) a.getData_venda());
+            stmt.setFloat(4,a.getValor_venda());
+            stmt.setInt(5, a.getId_animal());
+            stmt.executeUpdate();
+            JOptionPane.showConfirmDialog(null, "Atualizado com sucesso!");
+            
+        } catch (SQLException ex) {
+            JOptionPane.showConfirmDialog(null, " Erro ao atualizar: "+ex);
+        }finally{
+            ConnectionFactory.closeConnection(con, stmt);
+        }
+    }
+        
+        public static void delete(Animal a){
+        
+            Connection con = ConnectionFactory.getConnection();
+            String sql = "DELETE FROM animal WHERE idanimal=?";
+            PreparedStatement stmt = null;
+
+            try {
+                stmt = con.prepareStatement(sql);
+                stmt.setInt(1, a.getId_animal());
+                stmt.executeUpdate();
+                JOptionPane.showConfirmDialog(null, "Deletado com sucesso!");
+            } catch (SQLException ex) {
+                JOptionPane.showConfirmDialog(null, " Erro ao deletar: "+ex);
+            } finally{
+                ConnectionFactory.closeConnection(con, stmt);
+            }
+            
+        }
 }
--- src/br/com/farmcontrol/model/dao/AnimalDAODemo.java
+++ src/br/com/farmcontrol/model/dao/AnimalDAODemo.java
@@ -8,14 +8,40 @@ package br.com.farmcontrol.model.dao;
 import br.com.farmcontrol.model.vo.Animal;
 import br.com.farmcontrol.model.vo.LoteAves;
 import br.com.farmcontrol.model.vo.Mamifero;
+import java.sql.Date;
 
 /**
  *
  * @author viniciuslopes
  */
 public class AnimalDAODemo {
- 
-    Animal ani = new Mamifero();
     
+    public static void main(String[] args){
+        /*INSERT
+        Animal ani = new Mamifero();
+        ani.setRaca("Gado do Sul");
+        ani.setData_nasc_aquisicao(new Date(2020-1900, 10-1, 10));
+        AnimalDAO.create(ani);*/
+        
+        /*READ
+        for(Animal a:AnimalDAO.read()){
+            System.out.println(a);
+        }*/
+        
+        /*UPDATE
+        Animal ani = new Mamifero();
+        ani.setId_animal(2);
+        ani.setRaca("Gado Tradicional");
+        ani.setData_nasc_aquisicao(new Date(2020-1900, 9-1, 20));
+        ani.setData_venda(new Date(2020-1900, 12-1, 20));
+        ani.setValor_venda(5500);
+        AnimalDAO.update(ani);*/
+        
+        /*DELETE
+        Animal a = new Mamifero();
+        a.setId_animal(3);
+        AnimalDAO.delete(a);*/
+        
+    }
     
 }
