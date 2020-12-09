0b445aab9f1bae9ad7a3d8c39d86ad5fcf39b106
Jéssica Rosália <jessicarosalia678@gmail.com>
2020-12-09 04:10:17 -0300

    Criando controller do crud de ovo

--- src/br/com/farmcontrol/controller/ovo/CRUDOvoController.java
+++ src/br/com/farmcontrol/controller/ovo/CRUDOvoController.java
@@ -1,66 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.farmcontrol.controller.ovo;
 
-import br.com.farmcontrol.model.dao.AnimalDAO;
-import br.com.farmcontrol.model.dao.LoteAvesDAO;
-import br.com.farmcontrol.model.dao.OvoDao;
-import br.com.farmcontrol.model.dao.ReproducaoDAO;
-import br.com.farmcontrol.model.vo.Animal;
-import br.com.farmcontrol.model.vo.LoteAves;
-import br.com.farmcontrol.model.vo.Ovo;
 import java.net.URL;
 import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+
+import javax.swing.JOptionPane;
+
+import br.com.farmcontrol.model.dao.OvoDao;
+import br.com.farmcontrol.model.dao.ReproducaoDAO;
+import br.com.farmcontrol.model.vo.Animal;
+import br.com.farmcontrol.model.vo.LoteAves;
+import br.com.farmcontrol.model.vo.Ovo;
+import br.com.farmcontrol.model.vo.Reproducao;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
-/**
- * FXML Controller class
- *
- * @author viniciuslopes
- */
-public class CRUDOvoController implements Initializable {
-    
+public class CrudOvoController {
+
+    @FXML
+    private AnchorPane crudReprod;
+
     @FXML
     private Pane paneAnimais;
+    
+    @FXML
+    private TextField idovos;
 
     @FXML
-    private ListView<LoteAves> listAnimais;
+    private ListView<LoteAves> listLotes;
     
-    private List<LoteAves> animais = new ArrayList<>();
+    private List<LoteAves> lotes = new ArrayList();
     
-    private ObservableList<LoteAves> obsAnimal;
+    private ObservableList<LoteAves> obsLotes;
 
     @FXML
-    private ListView<Ovo> listOvos;
-    
+    private ListView<Ovo> listOvo;
+ 
     private List<Ovo> ovos = new ArrayList<>();
-            
-    private ObservableList<Ovo> obsOvos;
     
-    @FXML
-    private TextField idovo;
+    private ObservableList<Ovo> obsOvos;
 
     @FXML
     private TextField idanimal;
 
     @FXML
     private TextField quantidade;
-
-    @FXML
-    private TextField valor_unidade;
 
     @FXML
     private TextField data;
@@ -70,68 +65,166 @@ public class CRUDOvoController implements Initializable {
 
     @FXML
     private Button deletar;
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-        carregarAnimais();
+
+    @FXML
+    private Button adicionar;
+
+    @FXML
+    private TitledPane titledPane;
+
+    @FXML
+    private TextField cadastrar_id;
+
+    @FXML
+    private TextField cadastrar_quant;
+
+    @FXML
+    private TextField valor_unid;
+    
+    @FXML
+    private TextField cadastrar_valor_unid;
+
+    @FXML
+    private TextField cadastrar_data;
+
+    @FXML
+    private Button cadastrar_button;
+
+    @FXML
+    private Button cancelar_button;
+
+    @FXML
+    void atualizarOvos() {
+    	Ovo o = new Ovo();
+    	LoteAves la = listLotes.getSelectionModel().getSelectedItem();
+    	o.setLote(la);
+    	o.setData_producao(dataPadrao(data.getText()));
+        o.setQtd_ovos(Integer.parseInt(quantidade.getText()));
+        o.setValor_unidade(Float.parseFloat(valor_unid.getText()));
+        OvoDao.update(o);
+        carregarProducoes();
+    	
     }
     
-    public void deletarOvo(){
-        Ovo r = listOvos.getSelectionModel().getSelectedItem();
-        OvoDao.delete(r);
-        carregarOvos();
+
+    @FXML
+    void cadastrarOvo(ActionEvent event) {
+    	LoteAves la = listLotes.getSelectionModel().getSelectedItem();
+    	
+    	if(cadastrar_data.getText()==null || cadastrar_data.getText()=="" || cadastrar_quant.getText()==null || cadastrar_quant.getText()==null) {
+    		JOptionPane.showMessageDialog(null, "Os campos de data e quantidade são obrigatórios");
+    	}else {
+    		Ovo o = new Ovo();
+            o.setLote(la);
+            o.setData_producao(dataPadrao(cadastrar_data.getText()));
+            o.setQtd_ovos(Integer.parseInt(cadastrar_quant.getText()));
+            o.setValor_unidade(Float.parseFloat(valor_unid.getText()));
+            OvoDao.create(o);
+            
+            titledPane.setVisible(false);
+            
+            carregarProducoes();
+            
+            cadastrar_data.setText(null);
+            valor_unid.setText(null);
+            cadastrar_quant.setText(null);
+            
+            desativarComponentes(false);
+    	}
+    }
+
+    @FXML
+    void cancelarCadastro() {
+    	titledPane.setVisible(false);
+    	listLotes.setDisable(false);
+    	listOvo.setDisable(false);
     }
     
-    public void atualizarOvo(){
-        Ovo r = new Ovo();
-        LoteAves a =  listAnimais.getSelectionModel().getSelectedItem();
-        r.setLote(a);
-        r.setData_producao(dataPadrao(data.getText()));
-        r.setId_ovo(Integer.parseInt(idovo.getText()));
-        r.setQtd_ovos(Integer.parseInt(quantidade.getText()));
-        r.setValor_unidade(Float.parseFloat(valor_unidade.getText()));
-        OvoDao.update(r);
-        carregarOvos();
+
+    @FXML
+    void carregarProducoes() {
+    	LoteAves la = listLotes.getSelectionModel().getSelectedItem();
+    	ovos = OvoDao.read(la);
+    	obsOvos =FXCollections.observableArrayList(ovos);
+    	listOvo.setItems(obsOvos);
+    	
+    	idanimal.setText(String.valueOf(la.getId_animal()));
+    	limparCampos();
+    	atualizar.setDisable(true);
+    	deletar.setDisable(true);
+    	adicionar.setDisable(false);
+    	
     }
     
-    public void pegarDadosOvo(){
-        Ovo r = listOvos.getSelectionModel().getSelectedItem();
-        idovo.setText(String.valueOf(r.getId_ovo()));
-        quantidade.setText(String.valueOf(r.getQtd_ovos()));
-        valor_unidade.setText(String.valueOf(r.getValor_unidade()));
-        data.setText(String.valueOf(r.getData_producao()));
+
+    @FXML
+    void deletarOvos() {
+    	Ovo o = listOvo.getSelectionModel().getSelectedItem();
+    	OvoDao.delete(o);
+    	carregarProducoes();
     }
     
-    public void carregarOvos(){
-        LoteAves m = listAnimais.getSelectionModel().getSelectedItem();
-        ovos = OvoDao.read(m);
-        obsOvos = FXCollections.observableArrayList(ovos);
-        listOvos.setItems(obsOvos);
-        idanimal.setText(String.valueOf(m.getId_animal()));
-        limparCampos();
+    @FXML
+    void novaProd() {
+    	LoteAves la = listLotes.getSelectionModel().getSelectedItem();
+    	desativarComponentes(true);
+    	
+        titledPane.setVisible(true);
+        cadastrar_id.setText(String.valueOf(la.getId_animal()));
+    	
     }
     
-    public void carregarAnimais(){
-        /*animais = LoteAvesDAO.read();      
-        obsAnimal = FXCollections.observableArrayList(animais);
-        listAnimais.setItems(obsAnimal);    */ 
+
+    @FXML
+    void pegarDadosProducoes() {
+    	Ovo o = listOvo.getSelectionModel().getSelectedItem();
+    	idovos.setText(String.valueOf(o.getId_ovo()));
+    	quantidade.setText(String.valueOf(o.getQtd_ovos()));
+    	valor_unid.setText(String.valueOf(o.getValor_unidade()));
+    	data.setText(String.valueOf(o.getData_producao()));
+        atualizar.setDisable(false);
+        deletar.setDisable(false);
     }
-    
-    public void limparCampos(){
-        idovo.setText(null);
-        quantidade.setText(null);
-        valor_unidade.setText(null);
-        data.setText(null);
-    }
+
     
     public Date dataPadrao(String s){
         int ano = Integer.parseInt(s.substring(0,4));
         int mes = Integer.parseInt(s.substring(5,7));
         int dia = Integer.parseInt(s.substring(8,10));   
         return new Date(ano-1900, mes-1, dia);
-    } 
+    }
     
+    
+    public void limparCampos(){
+    	idovos.setText(null);
+    	idanimal.setText(null);
+      quantidade.setText(null);
+    	valor_unid.setText(null);
+        data.setText(null);
+    }
+    
+    
+
+    @FXML
+    void carregarLotes() {
+    	//lotes = LoteAvesDAO.read();     
+    //	obsOvos = FXCollections.observableArrayList(lotes);
+      //  listLotes.setItems(obsOvos); 
+    }
+    
+    public void desativarComponentes(Boolean b){
+        listLotes.setDisable(b);
+        listOvo.setDisable(b);
+        adicionar.setDisable(b);
+        atualizar.setDisable(b);
+        deletar.setDisable(b);
+    }
+    
+    
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        carregarLotes();
+    }
+
 }
+
